1a9651b34a9f5a03222c92175bc1c92bfe23b023
RS-Hades <mauricioserpas253@gmail.com>
2024-01-25 20:56:55 -0600

    Update of colors

--- JS-code/script.js
+++ JS-code/script.js
@@ -8,19 +8,23 @@ themeChecker.addEventListener("click", () => {
     const getVar = document.getElementsByClassName('light-theme')
   
     if (getVar.length > 0) {
-      root.style.setProperty('--main-color', 'rgb(250, 238, 253)');
+      root.style.setProperty('--tooltip-collapsed', 'rgb(11, 4, 12)');
+      root.style.setProperty('--lines-and-tooltip', 'white');
+      root.style.setProperty('--sidebar-hover', '#bababa');
       root.style.setProperty('--sidebar-background', 'rgb(250, 238, 253)');
+      root.style.setProperty('--sidebar-text-color', 'rgb(11, 4, 12)');
+      root.style.setProperty('--expand-button', 'rgb(11, 4, 12)');
+      root.style.setProperty('--main-color', 'rgb(250, 238, 253)');
       root.style.setProperty('--complementary-color', 'black');
-      root.style.setProperty('--sidebar-main-color', 'black');
-      root.style.setProperty('--text-link', '#525252');
-      root.style.setProperty('--expand-button', '#cecece');
     } else {
-      root.style.setProperty('--main-color', 'rgb(11, 4, 12)');
+      root.style.setProperty('--tooltip-collapsed', 'white');
+      root.style.setProperty('--lines-and-tooltip', 'rgb(11, 4, 12)');
+      root.style.setProperty('--sidebar-hover', '#353E47');
       root.style.setProperty('--sidebar-background', 'rgb(11, 4, 12)');
+      root.style.setProperty('--sidebar-text-color', 'white');
+      root.style.setProperty('--expand-button', 'white');
+      root.style.setProperty('--main-color', 'rgb(11, 4, 12)');
       root.style.setProperty('--complementary-color', 'white');
-      root.style.setProperty('--sidebar-main-color', 'white');
-      root.style.setProperty('--text-link', 'white');
-      root.style.setProperty('--expand-button', '#353E47');
     }
   });
 
