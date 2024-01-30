926a2c91964a577cf4b05e93a9a437003d8d3042
RS-Hades <mauricioserpas253@gmail.com>
2024-01-29 21:13:33 -0600

    advanced in the carrousel

--- JS-code/script-carrousel.js
+++ JS-code/script-carrousel.js
@@ -1,20 +1,38 @@
 const carrouselContainer = document.getElementById('carrousel-container');
 const carrousel = document.getElementById('carrousel');
-const buttonLeft = document.getElementById('button-left');
-const buttonRight = document.getElementById('button-right');
+const buttonLeft = document.getElementById('button-projects-left');
+const buttonRight = document.getElementById('button-projects-right');
 
-const direction = {
+const carrouselElements = document.getElementsByClassName('cards-projects');
+
+const rootStyles = document.querySelector(':root');
+
+const directionCarrousel = {
     right: 'right',
     left: 'left'
 };
 
-const moveSlide = (direction) => {
-    if (direction === direction.left) {
+const getTransformValue = () => Number(getComputedStyle(document.getElementById('carrousel')).getPropertyValue('--carrousel-transform').replace('px', ''));
 
-    } else if (direction === direction.right) {
-        
+let counter = 0;
+const reorderCarrousel = () => {
+    if (counter === carrouselElements.length - 1) {
+        carrousel.appendChild(carrousel.firstElementChild);
     }
 }
 
-buttonRight.addEventListener('click', ()=>moveSlide(direction.right));
-buttonLeft.addEventListener('click', ()=>moveSlide(direction.left));
+const moveSlide = (direction) => {
+    const transformValue = getTransformValue();
+    if (direction === directionCarrousel.left) {
+        rootStyles.style.setProperty('--carrousel-transform', `${transformValue + 300}px`);
+        counter--;
+    } else if (direction === directionCarrousel.right) {
+        rootStyles.style.setProperty('--carrousel-transform', `${transformValue - 300}px`);
+        counter++;
+    };
+
+    reorderCarrousel();
+};
+
+buttonRight.addEventListener('click', ()=>moveSlide(directionCarrousel.right));
+buttonLeft.addEventListener('click', ()=>moveSlide(directionCarrousel.left));
