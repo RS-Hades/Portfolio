3668cc587b071df0e4d10d48ae2d1b28f7d9be61
Mauricio Lovo <mauricioserpas253@gmail.com>
2024-02-13 02:53:16 -0600

    ALMOOOOOOOST FINISHED

--- JS-code/script.js
+++ JS-code/script.js
@@ -8,23 +8,26 @@ themeChecker.addEventListener("click", () => {
     const getVar = document.getElementsByClassName('light-theme')
   
     if (getVar.length > 0) {
-      root.style.setProperty('--tooltip-collapsed', 'rgb(11, 4, 12)');
-      root.style.setProperty('--lines-and-tooltip', 'white');
-      root.style.setProperty('--sidebar-hover', '#bababa');
-      root.style.setProperty('--sidebar-background', 'rgb(250, 238, 253)');
-      root.style.setProperty('--sidebar-text-color', 'rgb(11, 4, 12)');
-      root.style.setProperty('--expand-button', 'rgb(11, 4, 12)');
       root.style.setProperty('--main-color', '#F2E6D0');
       root.style.setProperty('--complementary-color', '#170137');
+      root.style.setProperty('--second-color', '#75eadf');
+      root.style.setProperty('--sixth-color', '#ff8eb0');
     } else {
-      root.style.setProperty('--tooltip-collapsed', 'white');
-      root.style.setProperty('--lines-and-tooltip', 'rgb(30, 11, 32)');
-      root.style.setProperty('--sidebar-hover', '#353E47');
-      root.style.setProperty('--sidebar-background', 'rgb(30, 11, 32)');
-      root.style.setProperty('--sidebar-text-color', 'white');
-      root.style.setProperty('--expand-button', 'white');
       root.style.setProperty('--main-color', '#170137');
       root.style.setProperty('--complementary-color', '#F2E6D0');
+      root.style.setProperty('--second-color', '#ffd91b');
+      root.style.setProperty('--sixth-color', '#F235B0');
     }
   });
 
+// copy button
+const copyButton = document.getElementById('copy-button');
+const linkEmail = document.getElementById('contact-link-email');
+
+copyButton.addEventListener('click', () => {
+  navigator.clipboard.writeText(linkEmail.innerHTML);
+  copyButton.innerHTML = 'Copied';
+  setTimeout(function () {
+    copyButton.innerHTML = 'Copy';
+  }, 1000);
+});
